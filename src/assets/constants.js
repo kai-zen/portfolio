65877bcb789475f5011f826298f41e00034e03b8
ali razipur <razipurali@gmail.com>
2022-10-03 12:48:59 +0330

    Added projects data

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -12,3 +12,135 @@ export const pages = [
     path: "/skills",
   },
 ];
+
+export const projectsData = [
+  {
+    id: 1,
+    title: "Dobaare",
+    subprojects: [
+      {
+        title: "Admin panel",
+        address: "http://panel.dobaare.com",
+        stack: ["React", "Tailwind", "Redux"],
+      },
+      {
+        title: "PWA",
+        address: "http://app.dobaare.com",
+        stack: ["React", "Tailwind", "Redux"],
+      },
+      {
+        title: "Landing page",
+        address: "http://dobaare.com",
+        stack: ["Next JS", "Material UI"],
+      },
+      {
+        title: "Blog",
+        address: "http://dobaare.com/blog",
+        stack: ["Next JS", "Material UI"],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Jib",
+    subprojects: [
+      {
+        title: "PWA",
+        address: "https://app.jibrasht.com",
+        stack: ["React", "Sass", "Redux"],
+      },
+      {
+        title: "ReactNative mobile app",
+        address:
+          "https://play.google.com/store/apps/details?id=com.clientjibapp",
+        stack: ["React Native", "Redux"],
+      },
+      {
+        title: "Landing page",
+        address: "https://jibrasht.com",
+        stack: ["Next JS", "Material UI"],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Challesh",
+    subprojects: [
+      {
+        title: "PWA",
+        address: "https://challesh.ir",
+        stack: ["React", "Material UI", "Redux"],
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Hadid HR management",
+    subprojects: [
+      {
+        title: "PWA",
+        address: "https://gitlab.com/omid36163413/hadidhrmfrontend",
+        stack: ["React", "Material UI", "Redux"],
+      },
+      {
+        title: "Admin panel",
+        address: false,
+        stack: ["React", "Material UI", "Redux"],
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Luna clothes shop",
+    subprojects: [
+      {
+        title: "PWA",
+        address: false,
+        stack: ["Next JS", "Tailwind", "Typesctipt", "Redux"],
+      },
+      {
+        title: "Admin panel",
+        address: "https://github.com/kai-zen/luna_panel",
+        stack: ["React", "Material UI", "GraphQL", "Redux"],
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Food Palace v2",
+    subprojects: [
+      {
+        title: "PWA with React",
+        address: "https://food-palace-2.netlify.app",
+        stack: ["React", "Javascript", "Material UI", "Redux"],
+      },
+      {
+        title: "Fullstack CMS with Next JS",
+        address: "https://github.com/kai-zen/next-food-palace-2",
+        stack: ["Next JS", "Material UI", "Redux", "MongoDB", "mongoose"],
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Food Palace",
+    subprojects: [
+      {
+        title: "PWA with React",
+        address: "https://food-palace-1.netlify.app",
+        stack: ["React", "Bootstrap", "React Context"],
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "Boring to do list",
+    subprojects: [
+      {
+        title: "Todo list with Redux",
+        address: "https://my-todo-list-redux.netlify.app",
+        stack: ["React", "CSS", "Redux"],
+      },
+    ],
+  },
+];
